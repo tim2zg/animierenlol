38aab480ba955c943bb65a243e62218a580d8e46
Your Name <ccu.schaller@outlook.com>
2022-01-12 08:51:49 +0100

    main runner progress

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import turtle
 
+import krankenwagen
+import weg
+
+run = True
+
+krankenwagenpositions = []
+
 
 def runner():
-    pass
+    while run:
+        for i in krankenwagenpositions:
+            weg.weg()
+            turtle.goto(i[0], i[1])
+            krankenwagen.krankenwagen()
+
 
 runner()
--- weg.py
+++ weg.py
@@ -1,8 +1,2 @@
-import turtle
-
-
 def weg():
     pass
-
-
-
